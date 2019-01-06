5684479a7b54c7828a672b929f125c9de14a5735
Gurcan Yavuz <gurcan.yavuz@yandex.com>
2019-01-06 11:04:42 +0300

    first commit

---
+++ CloudDocScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudDocScanner
+//
+//  Created by Gurcan Yavuz on 6.01.2019.
+//  Copyright © 2019 Madduck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
